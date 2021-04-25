51a4a8c6680adae2ed35e8a71304b90d7d2ac59e
DESKTOP-IAQ2P78\hn <1441374696@qq.com>
2021-04-25 17:39:54 +0800

    数组之中和为目标值的两个数

---
+++ demohn/demohn-web/src/main/java/com/hn/example/demohn/web/leetcode/TwoSum.java
@@ -0,0 +1,15 @@
+public class TwoSum {
+    public int[] twoSum(int[] num,int target) {
+        Map<Integer,Integer> cache = new HashMap<>();
+        if (num.length<2) {
+            throw new IllegalArgumentException("当前数组长度小于2");
+        }
+        for (int i=0;i<num.length;++i) {
+            if (cache.containsKey(target - num[i])) {
+                return new int[]{cache.get(num[i],i)};
+            }
+            cache.put(num[i],i);
+        }
+        throw new IllegalArgumentException("未找到当前数组之和为target");
+    }
+}
