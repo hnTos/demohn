61bbf79840a73f927f6089306921fef85ee395fb
余庆 <liyabin@mockuai.com>
2021-04-29 16:24:20 +0800

    给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效

---
+++ demohn/demohn-web/src/main/java/com/hn/example/demohn/web/leetcode/CharacterValid.java
@@ -0,0 +1,40 @@
+package com.hn.example.demohn.web.leetcode;
+
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+/**
+ * @author yuqing
+ * @date 2021-04-29
+ * @description
+ */
+public class CharacterValid {
+    public boolean isValid(String s) {
+        int n = s.length();
+        if(n %2 != 0) {
+            return false;
+        }
+        Map<Character,Character> pairs = new HashMap<Character,Character>(){
+            {
+                put(')','(');
+                put('}','{');
+                put(']','[');
+            }
+        };
+        Deque<Character> stack = new LinkedList<Character>();
+        for(int i=0;i<n;++i) {
+            char ch = s.charAt(i);
+            if(pairs.containsKey(ch)) {
+                if(stack == null || stack.peek() != pairs.get(ch)) {
+                    return false;
+                }
+                stack.pop();
+            } else {
+                stack.push(ch);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
