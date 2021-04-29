2451e75909766872fca131a3d255cd8e7ceb56a2
余庆 <liyabin@mockuai.com>
2021-04-29 17:46:24 +0800

    合并两个有序链表

---
+++ demohn/demohn-web/src/main/java/com/hn/example/demohn/web/leetcode/MergeTwoLists.java
@@ -0,0 +1,52 @@
+package com.hn.example.demohn.web.leetcode;
+
+/**
+ * @author yuqing
+ * @date 2021-04-29
+ * @description
+ */
+public class MergeTwoLists {
+    class ListNode {
+       int val;
+       ListNode next;
+       ListNode() {}
+       ListNode(int val) { this.val = val; }
+       ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1 == null) {
+            return l2;
+        } else if(l2 == null) {
+            return l1;
+        } else if(l1.val <l2.val){
+            l1.next = mergeTwoLists(l1.next,l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1,l2.next);
+            return l2;
+        }
+    }
+    public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        ListNode prehead = new ListNode(-1);
+
+        ListNode prev = prehead;
+
+        while (l1 != null && l2 != null) {
+            if(l1.val <= l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+        if (l1 != null) {
+            prev.next = l1;
+        }
+        if (l2 != null) {
+            prev.next = l2;
+        }
+        return prehead.next;
+    }
+}
